eff1a81ea551d6f5beb0d18593bfbb4d61a81209
Malcolm Maturen <malc@umich.edu>
2019-12-21 13:20:39 -0500

    placement creation, selection, and styling for index

--- placemaster/views/index.py
+++ placemaster/views/index.py
@@ -3,6 +3,9 @@ import flask
 from flask.views import View
 from flask_login import login_required
 
+from placemaster.model import query, db
+from placemaster.model.placement import Placement
+
 
 class IndexView(View):
     """Class based view for index page."""
@@ -15,9 +18,16 @@ class IndexView(View):
     def dispatch_request(self):
         """Override default View function.
 
-        See https://flask.palletsprojects.com/en/1.1.x/views/ for more info.
+        See https://flask.palletsprojects.com/en/1.1.x/views/ for more info
+        on pluggable views.
+
+        Lists all placements for this user,
+        or allows creation of new placement.
         """
-        return flask.render_template(IndexView.template)
+        context = {}
+        context['placements'] = query(Placement).all()
+        print(context['placements'])
+        return flask.render_template(IndexView.template, **context)
 
 
 __all__ = ['IndexView']
--- placemaster/views/placement.py
+++ placemaster/views/placement.py
@@ -2,24 +2,48 @@
 import flask
 from flask.views import View
 from flask_login import login_required
-from placemaster.model import query
+from placemaster.model import query, db
+from placemaster.model.placement import Placement
 
 
 class PlacementView(View):
     """Class-based view for placement page."""
 
-    url = '/placement/<id>'
+    methods = ['GET', 'POST']
+    url = '/placement/<id>/'
     endpoint = 'placement'
     template = 'placement.html'
 
-    def dispatch_request(self):
+    def dispatch_request(self, id):
         """Override default View function.
 
         See https://flask.palletsprojects.com/en/1.1.x/views/ for more info.
         """
         context = {}
-        context['placements'] = query(Placement).all()
-        return flask.render_template(PlacementView.template)
+        context['placement'] = query(Placement).filter_by(
+            id=id).first()
+        return flask.render_template(PlacementView.template, **context)
 
 
-__all__ = ['PlacementView']
+class CreatePlacement(View):
+    """Class-based endpoint for placement creation."""
+
+    methods = ['POST']
+    url = '/placement/create/'
+    endpoint = 'create_placement'
+    template = 'placement.html'
+
+    def dispatch_request(self):
+        """Handle form POST action for account creation."""
+        context = {}
+        newPlacement = Placement()
+        newPlacement.name = flask.request.form['placementTitle']
+        print(newPlacement.name)
+        db.session.add(newPlacement)
+        db.session.commit()
+        return flask.redirect(
+            flask.url_for('placement', id=newPlacement.id)
+        )
+
+
+__all__ = ['PlacementView', 'CreatePlacement']
